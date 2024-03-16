4b63093b7a89087fa0591dc5e68a301599e7fb35
joyjd <joydas1611@gmail.com>
2024-03-16 21:59:26 +0100

    draft push

--- client/src/pages/Pets.js
+++ client/src/pages/Pets.js
@@ -16,23 +16,40 @@ query AllPets {
 }
 `
 
+const NEW_PET = gql`
+    mutation CreateAPet($newPet: NewPetInput!) {
+      addPet(input: $newPet) {
+        id
+        name
+        type
+        img
+      }
+    }
+`
+
 export default function Pets() {
   const [modal, setModal] = useState(false)
   const { data, loading, error } = useQuery(ALL_PETS_QUERY);
+  const [createPet, newPet] = useMutation({ ...NEW_PET })
 
   const onSubmit = input => {
     setModal(false)
+    createPet({
+      variables: {
+        newPet: input
+      }
+    })
   }
 
   if (modal) {
     return <NewPetModal onSubmit={onSubmit} onCancel={() => setModal(false)} />
   }
 
-  if (loading) {
+  if (loading || newPet.loading) {
     return <Loader />
   }
 
-  if (error) {
+  if (error || newPet.error) {
     return <div>Error</div>
   }
   return (
